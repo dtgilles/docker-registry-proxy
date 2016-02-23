e0a8018777b2871a18ca71fe552361db7a88a524
dtgilles <dtgilles@t-online.de>
2016-02-23 07:42:38 +0100

    * a couple of little bug fixes iterating build.sh, a test procedure * introduced a gitolite docker container named dregit:   - permission management is central part of gitolite   - extensible by further commands like "My" * integration are currently not testified

--- test/build.sh
+++ test/build.sh
@@ -5,14 +5,61 @@ xset=""
 
 f=authorize/ca/bin/ca.script
 sh $xset $f create_tmp_ca  ||  exit $?
-ssh-keygen -t rsa -C "temporary root key" -P "" -f tmp/rootkey -b 4096
+[ -f tmp/rootkey ] \
+|| ssh-keygen -t rsa -C "temporary root key" -P "" -f tmp/rootkey -b 4096
 rootkey=$(cat tmp/rootkey.pub)
 
 sed -e "/RootKey=/  s|=.*|=$rootkey|" \
-       "/SSH_KEY=/  s|=.*|=$rootkey|" \
+    -e "/SSH_KEY=/  s|=.*|=$rootkey|" \
         docker-compose.yml > dcc.yml
 
 cp tmp/ca/intermediate/certs/ca-chain.cert.pem  rep/client-ca.crt
 cp tmp/ca/intermediate/crl/intermediate.crl.pem rep/client-crl
 
 docker-compose -f dcc.yml build
+
+docker-compose -f dcc.yml up -d dregit authorize  ; sleep 5
+if ! grep -iq "host  *dregit" $HOME/.ssh/config
+   then
+      (
+      echo
+      echo "Host          dregit"
+      echo "HostName      localhost"
+      echo "User          git"
+      echo "Port          1140"
+      echo "IdentityFile  $PWD/tmp/rootkey"
+      echo "StrictHostKeyChecking no"
+      ) >> $HOME/.ssh/config
+   fi
+if ! grep -iq "host  *authorize" $HOME/.ssh/config
+   then
+      (
+      echo
+      echo "Host          authorize"
+      echo "HostName      localhost"
+      echo "User          root"
+      echo "Port          1122"
+      echo "IdentityFile  $PWD/tmp/rootkey"
+      echo "StrictHostKeyChecking no"
+      ) >> $HOME/.ssh/config
+   fi
+if ! grep -iq 'host  *\*$' $HOME/.ssh/config
+   then
+      (
+      echo
+      echo "Host          *"
+      echo "StrictHostKeyChecking no"
+      ) >> $HOME/.ssh/config
+   fi
+##### if access is denied, please check admin-key in dregit-repo!!
+if [ -d tmp/gitadm ]
+   then
+      ln -s ../../tmp tmp/gitadm/. 2>/dev/null
+      (cd tmp/gitadm && git pull --rebase || exit 1)
+   else
+      git clone ssh://dregit/gitolite-admin tmp/gitadm
+   fi
+key=$(cat tmp/gitadm/keydir/automation.pub)
+[ -z "$key" ] && echo "no admin key found -- exit with error" >&2 && exit 1
+key='no-pty,no-port-forwarding,command="/etc/ca/bin/login"'" $key"
+(cd tmp/ca && tar -cf - intermediate* | ssh authorize "echo '$key' >> /root/.ssh/authorized_keys; cd \$CaBase && tar -xf -")
